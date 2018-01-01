d2b568b8d2e84aadf75720850f0aaa2ba828915c
Sarim Abbas <sarimabbas@sarims-macbook.lan>
2018-01-01 20:08:25 +0500

    minor

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
 import re
 import json
+import pprint
 from flask import Flask
+
+pp = pprint.PrettyPrinter(indent=4)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    print(os.environ)
-    print(process.env.MONGOLAB_URI)
+    pp.pprint(os.environ)
+    pp.pprint(process.env.MONGOLAB_URI)
     return "Hello world!"
 
 if __name__ == "__main__":
