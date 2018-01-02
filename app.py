c8e97f1b7c5bb9c0691c7092d6b4d926f4412746
Sarim Abbas <sarimabbas@sarims-macbook.lan>
2018-01-02 11:11:18 +0500

    api + models

--- app.py
+++ app.py
@@ -4,34 +4,34 @@ import json
 import pprint
 
 from flask import Flask
+from flask.ext import restful
+
 from mongoengine import *
 from models import *
 
 pp = pprint.PrettyPrinter(indent=4)
 
 app = Flask(__name__)
+api = restful.Api(app)
 
 # connect to the database (currently mongodb, hoping for postgres later)
 app.config["DATABASE_URI"] = os.environ.get("MONGODB_URI")
 db = connect(db="thinkspace", host=app.config["DATABASE_URI"])
 
+# client
 @app.route("/")
-def hello():
+def index():
+    return "Hello world!
 
-    post1 = TextPost(title='Using MongoEngine', content='See the tutorial')
-    post1.tags = ['mongodb', 'mongoengine']
-    post1.save()
+# API
+class Test(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-    yalie = YaleStudent(email="hello@yale.edu", first_name="Sar", last_name="Abb", net_id="sa857")
-    yalie.save()
-
-    return "Hello world! {}".format(TextPost.objects.count())
-
-@app.route('/<string:path>', methods=['GET'])
-def root(path):
-    return 'You requested: /' + path
-
+api.add_resource(Users, '/api/users')
+api.add_resource(User, '/api/user/<string:username>')
+api.add_resource(Test, '/api/test')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True) # turn off debug for production
--- models.py
+++ models.py
@@ -1,20 +1,51 @@
 import datetime
 from mongoengine import *
 
-class BlogPost(Document):
-    title = StringField(required=True, max_length=200)
-    posted = DateTimeField(default=datetime.datetime.utcnow)
-    tags = ListField(StringField(max_length=50))
-    meta = {'allow_inheritance': True}
+# EmbeddedDocument : cannot exist independently
+# ReferenceField : allows reuse of a class
+# Inherited Classes : allows specialisation of a class
 
-class TextPost(BlogPost):
-    content = StringField(required=True)
+# extensible class to manage external links
+class Links(EmbeddedDocument):
+    github = URLField()
+    linkedin = URLField()
+    facebook = URLField()
+    twitter = URLField()
+    website = URLField()
 
+# generic user class
 class User(Document):
-    email = StringField(required=True)
+    email = EmailField(required=True)
+    username = StringField(max_length=50, required=True)
     first_name = StringField(max_length=50)
     last_name = StringField(max_length=50)
+    image = ImageField()
+    projects = ListField(StringField())
+    links = EmbeddedDocumentField(Links)
+    hearts = IntField()
+    hearted = ListField(ReferenceField(Project))
+    created = DateTimeField(default=datetime.datetime.utcnow)
     meta = {'allow_inheritance': True}
 
+# extended user class
 class YaleStudent(User):
     net_id = StringField(required=True, max_length=10)
+
+class Comment(EmbeddedDocument):
+    commenter = ReferenceField(User) # what happens when a user is deleted?
+    content = StringField()
+    created = DateTimeField(default=datetime.datetime.utcnow)
+
+class Update(EmbeddedDocument):
+    content = StringField()
+    created = DateTimeField(default=datetime.datetime.utcnow)
+
+class Project(Document):
+    title = StringField(max_length=50)
+    subtitle = StringField()
+    category = StringField()
+    founders = ListField(ReferenceField(User))
+    comments = ListField(EmbeddedDocumentField(Comment))
+    updates = ListField(EmbeddedDocumentField(Update))
+    created = DateTimeField(default=datetime.datetime.utcnow)
+    hearts = IntField()
